69d3096ff3d4cda2605a9b57bac195d49ba2dab3
JClements56 <fbird57L@hotmail.com>
2019-03-21 20:30:30 -0400

    added for loop

--- oddities.java
+++ oddities.java
@@ -2,7 +2,20 @@ import java.util.*;
 
 public class Oddities{
 
-	public 
+	public static void main(String[] args){
+	
+		int in = new scanner(System.in);
+
+		int testCase = args[0];
+
+		for(int i = 0; i < testCase; i++){
+			int number = args[i];
+
+			boolean isEven = False;
+
+			
+		
+	}
 
 
 
